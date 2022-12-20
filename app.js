3b3060847d7d87ac9c12ddc973a1e313b6717eab
Berkan Özgür <berkanozgur@hotmail.com>
2022-12-20 19:12:22 +0300

    Template and Template engine added.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express')
+const ejs = require('ejs');
+const path = require('path');
 
 const app = express();
 
-const photo = {
-    id: 1,
-    name: "Photo Name",
-    desc: "Photo Desc"
-}
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send(photo)
+    res.render('index');
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/add', (req, res) => {
+    res.render('add')
 })
 
 const port = 3000;
