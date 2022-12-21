b09ad22cc5c29f83f6d59e71a337c240b856981d
Berkan Özgür <berkanozgur@hotmail.com>
2022-12-21 22:37:52 +0300

    Database connections added.

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo')
 
 const app = express();
 
+mongoose.set('strictQuery', true);
+mongoose.connect(
+    "mongodb://127.0.0.1/pcat-test-db",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({})
+    res.render('index', {
+        photos
+    });
 })
 app.get('/about', (req, res) => {
     res.render('about')
@@ -17,7 +32,10 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
     res.render('add')
 })
-
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect('/')
+})
 const port = 3000;
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı...`)
