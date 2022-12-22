5474f0847c69c603ef4553f8964b3b58dfcfb8ac
Berkan Özgür <berkanozgur@hotmail.com>
2022-12-22 17:28:04 +0300

    Crud and MVC arrangements are done.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo')
+const photoController = require("./controllers/photoControllers")
+const pageController = require("./controllers/pageController")
+
 
 const app = express();
 
@@ -19,23 +22,20 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileUpload())
+app.use(methodOverride('_method', {
+    methods: ['POST', 'GET']
+}))
 
-app.get('/', async (req, res) => {
-    const photos = await Photo.find({})
-    res.render('index', {
-        photos
-    });
-})
-app.get('/about', (req, res) => {
-    res.render('about')
-})
-app.get('/add', (req, res) => {
-    res.render('add')
-})
-app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/')
-})
+app.get('/', photoController.getAllPhotos)
+app.get('/photos/:id', photoController.getPhoto)
+app.post('/photos', photoController.createPhoto)
+app.put('/photos/:id', photoController.updatePhoto)
+app.delete('/photos/:id', photoController.deletePhoto)
+app.get('/about', pageController.getAboutPage)
+app.get('/add', pageController.getAddPage)
+app.get('/photos/edit/:id', pageController.getEditPage)
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı...`)
